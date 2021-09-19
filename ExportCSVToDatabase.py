9dc67ab8899ada89919dccb65deec001a0753dca
Alex Denford <pantherNZ@users.noreply.github.com>
2021-09-19 21:46:20 +1200

    Setup CSV for trick list with python script to import into the SQL database

---
+++ ExportCSVToDatabase.py
@@ -0,0 +1,30 @@
+import csv, sqlite3
+
+con = sqlite3.connect("Assets/StreamingAssets/Database.db")
+cur = con.cursor()
+
+cur.execute("DROP TABLE IF EXISTS Tricks_Backup")
+cur.execute("CREATE TABLE Tricks_Backup AS SELECT * FROM Tricks")
+cur.execute("DELETE FROM Tricks")
+
+with open('Tricklist.csv','r') as file:
+    # csv.DictReader uses first line in file for column headings by default
+    csv_reader = csv.DictReader(file)
+    column_names = []
+
+    for row in csv_reader:
+        values = ','.join([f'"{x}"' for x in row.values()])
+        sql = f"INSERT INTO Tricks VALUES ({values})"
+        #print(sql)
+        cur.execute(sql)
+
+        if len(column_names) == 0:
+            column_names = row.keys()
+
+    for column in column_names:
+        sql = f"UPDATE Tricks SET {column} = NULL WHERE {column} = 'INVALID'"
+        #print(sql)
+        cur.execute(sql)
+
+con.commit()
+con.close()
